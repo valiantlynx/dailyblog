d3beca48698c89e5046c9e062e3fcbae3d93298f
valiantlynx <gormerykombo@gmail.com>
2022-05-28 23:44:39 +0200

    everything still works. i have made the websitemore stable but the posts have not yet been opened on new pages each

--- app.js
+++ app.js
@@ -3,8 +3,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
+let posts = [];
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const homeStartingContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper."
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
@@ -15,29 +17,53 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+//home
 app.get("/", function(req, res) {
-
-    res.render("home", { homeContent: homeStartingContent });
+    res.render("home", {
+        homeContent: homeStartingContent,
+        posts: posts
+    });
 });
 
+//about
 app.get("/about", function(req, res) {
     res.render("about", { aboutPageContent: aboutContent });
 });
 
+//contact
 app.get("/contact", function(req, res) {
     res.render("contact", { contactPageContent: contactContent });
 });
 
+//compose
+app.get("/compose", function(req, res) {
+    res.render("compose");
+});
+app.post("/compose", function(req, res) {
+    const post = {
+        title: req.body.postTitle,
+        body: req.body.postText
+    };
+    posts.push(post);
+    res.redirect("/");
+});
 
+//using route parameters
+app.get("/posts/:postName", function(req, res) {
+    const requestedTitle = _.lowerCase([req.params.postName]);
+    posts.forEach(function(post) {
+        const storedTitle = _.lowerCase([post.title]);
+        if (requestedTitle === storedTitle) {
 
+            console.log("Match found")
+        } else {
+            console.log("Match not found")
+        }
+    });
 
+});
 
-
-
-
-
-
-
+//port listening
 app.listen(3000, function() {
     console.log("Server started on port 3000");
 });
