6d05be356af3d07dba5c0ac712c6204839359306
valiantlynx <gormerykombo@gmail.com>
2022-06-02 06:46:19 +0200

    this kind of works ata this point i just need to make the stored content show

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ const _ = require("lodash");
 //mongoose.connect('mongodb+srv://admin-valiantlynx:valiantlynx@cluster0.lujhkmj.mongodb.net/dailyBlog');
 mongoose.connect('mongodb://localhost:27017/dailyBlog');
 
-let posts = [];
-
 const homeStartingContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper."
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
@@ -22,6 +20,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+//default content for home, about, contact
 const blogSchema = {
     name: String,
     blog: String
@@ -44,8 +43,15 @@ const contact = new Blog({
     blog: contactContent
 });
 
-const defaultItems = [];
+//content posts
 
+const listSchema = {
+    name: String,
+    items: [blogSchema]
+}
+
+const Post = mongoose.model("Post", listSchema);
+let posts = [];
 
 //home
 app.get("/", function(req, res) {
@@ -56,12 +62,13 @@ app.get("/", function(req, res) {
                 if (err) {
                     console.log(err);
                 } else {
-                    console.log("Successfully inserted home item into collection");
+                    console.log("Successfully inserted home home into collection");
                 }
             });
             res.redirect("/");
 
         } else {
+            console.log(posts);
             res.render("home", {
                 homeTitle: foundItem[0].name,
                 homeContent: foundItem[0].blog,
@@ -80,7 +87,7 @@ app.get("/about", function(req, res) {
                 if (err) {
                     console.log(err);
                 } else {
-                    console.log("Successfully inserted about item into collection");
+                    console.log("Successfully inserted about about into collection");
                 }
             });
             res.redirect("/");
@@ -102,7 +109,7 @@ app.get("/contact", function(req, res) {
                 if (err) {
                     console.log(err);
                 } else {
-                    console.log("Successfully inserted contact item into collection");
+                    console.log("Successfully inserted contact contact into collection");
                 }
             });
             res.redirect("/");
@@ -118,13 +125,27 @@ app.get("/contact", function(req, res) {
 
 //compose
 app.get("/compose", function(req, res) {
+
     res.render("compose");
 });
+
 app.post("/compose", function(req, res) {
+    let title = req.body.postTitle
+    let body = req.body.postText
+
     const post = {
-        title: req.body.postTitle,
-        body: req.body.postText
+        title: title,
+        body: body
     };
+
+    console.log(post);
+    Blog.insertMany(post, function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Successfully inserted contact contact into collection");
+        }
+    });
     posts.push(post);
     res.redirect("/");
 });
@@ -134,20 +155,19 @@ app.post("/compose", function(req, res) {
 app.get("/posts/:postName", function(req, res) {
     const requestedTitle = _.lowerCase(req.params.postName);
 
-    posts.forEach(function(post) {
-        const storedTitle = _.lowerCase(post.title);
-        if (requestedTitle === storedTitle) {
-            res.render("post", {
-                title: post.title,
-                body: post.body
-            });
+    Blog.find({ name: requestedTitle }, function(err, foundItem) {
+        console.log(foundItem);
 
-        }
+        res.render("post", {
+            title: foundItem.name,
+            body: foundItem.blog,
+        });
+
     });
-
-
 });
+
 app.post("/posts", function(req, res) {
+
     res.redirect("/posts");
 
 })
